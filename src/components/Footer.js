7edbdd4dcf90955932173076adf4ed44322fde8e
JRingterm <ghwns3712@naver.com>
2023-01-20 21:55:32 +0900

    초안

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import styles from "../css/layout.module.css"
+
+function Footer() {
+    return(
+        <footer className={styles.footer}>
+            <hr />
+            <div>
+                <p>ghwns3712@naver.com</p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ src/routers/Pkrecord.js
@@ -0,0 +1,22 @@
+import divstyles from "../css/Div.module.css";
+import Footer from "../components/Footer";
+import styles from "../css/styles.css";
+function Pkrecord() {
+    return(
+        <>
+        <div className={divstyles.maindiv}>
+            <h1>실전개체 기록소</h1>
+            
+            <div className={divstyles.contentsdiv}>
+                
+            </div>
+            
+
+        </div>
+
+        <Footer />
+        </>
+    );
+}
+
+export default Pkrecord;
